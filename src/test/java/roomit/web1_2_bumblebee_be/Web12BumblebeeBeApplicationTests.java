b93a87b160eb50b6853b686c0c207580443afbff
Si-rauis <kkyd7189@gmail.com>
2024-11-18 15:07:44 +0900

    🎉start : 개발환경 세팅

---
+++ src/test/java/roomit/web1_2_bumblebee_be/Web12BumblebeeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package roomit.web1_2_bumblebee_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web12BumblebeeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
