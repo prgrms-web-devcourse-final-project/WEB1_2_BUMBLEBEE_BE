84b83ec3b890338190958933ca4d889c2f831056
CryingPerson <opnice12@naver.com>
2024-11-19 20:47:52 +0900

    ♻️ Refactor : DTO 수정

--- src/main/java/roomit/web1_2_bumblebee_be/domain/member/response/ErrorResponse.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/member/response/ErrorResponse.java
@@ -2,6 +2,7 @@ package roomit.web1_2_bumblebee_be.domain.member.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import roomit.web1_2_bumblebee_be.global.error.ErrorCode;
 
 import java.util.Map;
 
@@ -10,12 +11,14 @@ public class ErrorResponse {
 
     private final String code;
     private final String message;
+    private int status;
 
 
     @Builder
-    public ErrorResponse(String code, String message, Map<String, String> validation) {
-        this.code = code;
-        this.message = message;
+    public ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+        this.status = errorCode.getStatus();
     }
 
 }
