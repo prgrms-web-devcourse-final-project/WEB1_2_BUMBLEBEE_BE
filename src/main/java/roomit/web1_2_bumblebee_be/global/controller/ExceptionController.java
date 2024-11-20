4e90b9002b29aec946ab516cd0d7ab205530d206
CryingPerson <opnice12@naver.com>
2024-11-20 11:47:27 +0900

    ✨ Feat : Validation 오류 발생시 ExceptionController 클래스 추가

--- src/main/java/roomit/web1_2_bumblebee_be/global/controller/ExceptionController.java
+++ src/main/java/roomit/web1_2_bumblebee_be/global/controller/ExceptionController.java
@@ -1,7 +1,11 @@
 package roomit.web1_2_bumblebee_be.global.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import roomit.web1_2_bumblebee_be.global.error.ErrorResponse;
 import roomit.web1_2_bumblebee_be.global.exception.commonException;
@@ -17,5 +21,20 @@ public class ExceptionController {
                 .build();
 
         return ResponseEntity.status(statusCode).body(response);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse.ErrorValidation> exceptionHandler(MethodArgumentNotValidException e) {
+
+        ErrorResponse.ErrorValidation response = ErrorResponse.ErrorValidation.builder()
+                .code("400")
+                .message("잘못된 요청입니다.")
+                .build();
+        for(FieldError fieldError : e.getFieldErrors()){
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(400).body(response);
+
     }
 }
