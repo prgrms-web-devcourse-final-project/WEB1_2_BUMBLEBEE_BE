6fcf852918bd5e338dc4a770d3eb25e5878ed3b4
Si-rauis <kkyd7189@gmail.com>
2024-11-20 14:22:22 +0900

    ✨ Feat : ErrorCode에 Business 예외 추가

--- src/main/java/roomit/web1_2_bumblebee_be/global/error/ErrorCode.java
+++ src/main/java/roomit/web1_2_bumblebee_be/global/error/ErrorCode.java
@@ -1,7 +1,16 @@
 package roomit.web1_2_bumblebee_be.global.error;
 
 public enum ErrorCode {
-    MEMBER_NOT_FOUND(400, "M001", "존재 하지 않는 회원입니다.");
+    /*Login*/
+    BUSINESS_NOT_REGISTER(400, "L006", "사업자 등록에 실패했습니다."),
+
+    /*Member*/
+    MEMBER_NOT_FOUND(400, "M001", "존재 하지 않는 회원입니다."),
+
+    /*Business*/
+    BUSINESS_NOT_FOUND(400, "B003", "존재 하지 않는 사업자입니다."),
+    BUSINESS_NOT_MODIFY(400, "B001", "사업자 수정에 실패했습니다."),
+    BUSINESS_NOT_DELETE(400, "B002", "사업자 탈퇴에 실패했습니다.");
 
 
     private  String code;
