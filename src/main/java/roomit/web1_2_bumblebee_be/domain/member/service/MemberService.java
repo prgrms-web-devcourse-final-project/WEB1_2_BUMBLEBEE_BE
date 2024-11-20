beca05ccb23f5440148a937f07ee19a3624527b0
bung-dev <wndgh26@gmail.com>
2024-11-20 14:47:36 +0900

    :bug: fix : 충돌 해결

--- src/main/java/roomit/web1_2_bumblebee_be/domain/member/service/MemberService.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/member/service/MemberService.java
@@ -17,19 +17,19 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder; // BCryptPasswordEncoder 추가
 
-    public void signupMember(MemberRegisterRequest memberRequest) {
-        Member member = Member.builder()
-                .nickName(memberRequest.getNickName())
-                .age(memberRequest.getAge())
-                .sex(memberRequest.getSex())
-                .pwd(bCryptPasswordEncoder.encode(memberRequest.getPwd())) //비밀번호 암호화 추가
-                .email(memberRequest.getEmail())
-                .role(memberRequest.getRole())
-                .phoneNumber(memberRequest.getPhoneNumber())
-                .build();
-
-        memberRepository.save(member);
-    }
+//    public void signupMember(MemberRegisterRequest memberRequest) {
+//        Member member = Member.builder()
+//                .nickName(memberRequest.getNickName())
+//                .age(memberRequest.getAge())
+//                .sex(memberRequest.getSex())
+//                .pwd(bCryptPasswordEncoder.encode(memberRequest.getPwd())) //비밀번호 암호화 추가
+//                .email(memberRequest.getEmail())
+//                .role(memberRequest.getRole())
+//                .phoneNumber(memberRequest.getPhoneNumber())
+//                .build();
+//
+//        memberRepository.save(member);
+//    }
 
     public MemberResponse read(Long memberId) {
         Member member = memberRepository.findById(memberId)
