59223b1016baf048ec908e7df8319e3c5e17b2ca
Si-rauis <kkyd7189@gmail.com>
2024-11-20 14:21:58 +0900

    ✨ Feat : Business 예외처리 추가

--- src/main/java/roomit/web1_2_bumblebee_be/domain/business/exception/BusinessNotDelete.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/business/exception/BusinessNotDelete.java
@@ -3,8 +3,8 @@ package roomit.web1_2_bumblebee_be.domain.business.exception;
 import roomit.web1_2_bumblebee_be.global.error.ErrorCode;
 import roomit.web1_2_bumblebee_be.global.exception.commonException;
 
-public class BusinessNotFound extends commonException {
+public class BusinessNotDelete extends commonException {
 
-    public BusinessNotFound() {super(ErrorCode.BUSINESS_NOT_FOUND);}
+    public BusinessNotDelete() {super(ErrorCode.BUSINESS_NOT_DELETE);}
 
 }
--- src/main/java/roomit/web1_2_bumblebee_be/domain/business/exception/BusinessNotModify.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/business/exception/BusinessNotModify.java
@@ -3,8 +3,8 @@ package roomit.web1_2_bumblebee_be.domain.business.exception;
 import roomit.web1_2_bumblebee_be.global.error.ErrorCode;
 import roomit.web1_2_bumblebee_be.global.exception.commonException;
 
-public class BusinessNotFound extends commonException {
+public class BusinessNotModify extends commonException {
 
-    public BusinessNotFound() {super(ErrorCode.BUSINESS_NOT_FOUND);}
+    public BusinessNotModify() {super(ErrorCode.BUSINESS_NOT_MODIFY);}
 
 }
