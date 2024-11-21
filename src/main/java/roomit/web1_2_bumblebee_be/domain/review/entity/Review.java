211d58ed610290bed46676bf17f4059d6b7d41cc
CryingPerson <opnice12@naver.com>
2024-11-21 14:43:58 +0900

    ✨ Feat : Review Entity 추가

---
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/review/entity/Review.java
@@ -0,0 +1,68 @@
+package roomit.web1_2_bumblebee_be.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import roomit.web1_2_bumblebee_be.domain.member.entity.Member;
+import roomit.web1_2_bumblebee_be.domain.workplace.entity.Workplace;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "review")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id", nullable = false)
+    private Long reviewId;
+
+    @Column(name = "review_content", nullable = false, length = 50)
+    private String reviewContent;
+
+    @Column(name = "review_rating", nullable = false)
+    private String reviewRating;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Setter
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workplace_id", nullable = false)
+    private Workplace workplace;
+
+    @Builder
+    public Review(String reviewContent, String reviewRating, Member member, Workplace workplace) {
+        this.reviewContent = reviewContent;
+        this.reviewRating = reviewRating;
+        this.createdAt = LocalDateTime.now();
+        this.member = member;
+        this.workplace = workplace;
+    }
+
+    public void changeReviewRating(String reviewRating) {
+        this.reviewRating = reviewRating;
+    }
+    public void changeReviewContent(String reviewContent) {
+        this.reviewContent = reviewContent;
+    }
+    public void changeCreatedAt() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
