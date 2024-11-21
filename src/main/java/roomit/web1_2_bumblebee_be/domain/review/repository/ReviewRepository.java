6cbea5a8dacec66ef75f64df22b5f1f96029939f
CryingPerson <opnice12@naver.com>
2024-11-21 14:44:07 +0900

    ✨ Feat : ReviewRepository 추가

---
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package roomit.web1_2_bumblebee_be.domain.review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import roomit.web1_2_bumblebee_be.domain.review.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
