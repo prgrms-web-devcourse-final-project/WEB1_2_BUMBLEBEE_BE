0a60e76fa1a3bed71fe57ef762bb7aaa4b8897e9
CryingPerson <opnice12@naver.com>
2024-11-21 16:05:40 +0900

    ✨ Feat : ReviewServiceTest  추가

--- src/test/java/roomit/web1_2_bumblebee_be/domain/review/service/ReviewServiceTest.java
+++ src/test/java/roomit/web1_2_bumblebee_be/domain/review/service/ReviewServiceTest.java
@@ -12,7 +12,9 @@ import roomit.web1_2_bumblebee_be.domain.member.entity.Role;
 import roomit.web1_2_bumblebee_be.domain.member.entity.Sex;
 import roomit.web1_2_bumblebee_be.domain.member.repository.MemberRepository;
 import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewRegisterRequest;
+import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewSearch;
 import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewUpdateRequest;
+import roomit.web1_2_bumblebee_be.domain.review.dto.response.ReviewResponse;
 import roomit.web1_2_bumblebee_be.domain.review.entity.Review;
 import roomit.web1_2_bumblebee_be.domain.review.repository.ReviewRepository;
 import roomit.web1_2_bumblebee_be.domain.workplace.entity.Workplace;
@@ -20,6 +22,7 @@ import roomit.web1_2_bumblebee_be.domain.workplace.repository.WorkplaceRepositor
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,7 +47,8 @@ class ReviewServiceTest {
         memberRepository.deleteAll();
         workplaceRepository.deleteAll();
     }
-//    @Test
+
+    //    @Test
 //    @DisplayName("리뷰 등록하기")
 //    @Transactional
 //    void test0() {
@@ -126,6 +130,7 @@ class ReviewServiceTest {
         assertEquals("좋은 장소네요", all.get(0).getReviewContent());
 
     }
+
     @Test
     @DisplayName("리뷰 수정하기")
     @Transactional
@@ -180,6 +185,7 @@ class ReviewServiceTest {
 //        assertEquals(1L, all.get(0).getWorkplace().getWorkplaceId());
 
     }
+
     @Test
     @DisplayName("리뷰 조회")
     @Transactional
@@ -225,6 +231,7 @@ class ReviewServiceTest {
         assertEquals("별점4개", all.get(0).getReviewRating());
 
     }
+
     @Test
     @DisplayName("리뷰 조회")
     @Transactional
@@ -266,6 +273,57 @@ class ReviewServiceTest {
         reviewService.remove(review.getReviewId());
 
         assertEquals(0, reviewRepository.findAll().size());
+
+    }
+
+    @Test
+    @DisplayName("리뷰 페이징")
+    @Transactional
+    void test5() {
+
+        Member member = Member.builder()
+                .memberAge(Age.TEN)
+                .memberSex(Sex.FEMALE)
+                .memberRole(Role.ROLE_USER)
+                .memberPwd("1111")
+                .memberEmail("이시현@Naver.com")
+                .memberPhoneNumber("010-33230-23")
+                .memberNickName("치킨유저")
+                .build();
+
+        memberRepository.save(member);
+
+        Workplace workplace = Workplace.builder()
+                .workplaceName("사업장")
+                .workplacePhoneNumber("010-1234-1234")
+                .workplaceDescription("사업장 설명")
+                .workplaceAddress("대한민국")
+                .profileImage(null)
+                .imageType(null)
+                .workplaceStartTime(LocalDateTime.of(2023, 1, 1, 9, 0))
+                .workplaceEndTime(LocalDateTime.of(2023, 1, 1, 18, 0))
+                .build();
+        workplaceRepository.save(workplace);
+
+        List<Review> list = IntStream.rangeClosed(0, 19).mapToObj(i -> Review.builder()
+                .reviewContent("치킨이 안보이네요.." + i)
+                .reviewRating("별점4개" + i)
+                .member(member)
+                .workplace(workplace)
+                .build()).toList();
+        ReviewSearch reviewSearch = ReviewSearch.builder()
+                .page(0)
+                .size(10)
+                .build();
+
+        reviewRepository.saveAll(list);
+
+        List<ReviewResponse> list1 = reviewService.getList(reviewSearch);
+
+        assertEquals(10, list1.size());
+        assertEquals("치킨이 안보이네요..19", list1.get(0).getReviewContent());
+
+
 
     }
 }
