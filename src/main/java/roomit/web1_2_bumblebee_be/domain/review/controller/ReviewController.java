60584271ac34880b34bc206a64c8bfab64dae2d5
CryingPerson <opnice12@naver.com>
2024-11-21 16:02:42 +0900

    ✨ Feat :  Review 페이징 처리 추가

--- src/main/java/roomit/web1_2_bumblebee_be/domain/review/controller/ReviewController.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/review/controller/ReviewController.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewRegisterRequest;
+import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewSearch;
 import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewUpdateRequest;
 import roomit.web1_2_bumblebee_be.domain.review.dto.response.ReviewResponse;
 import roomit.web1_2_bumblebee_be.domain.review.service.ReviewService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,16 +26,24 @@ public class ReviewController {
 
     @PutMapping("/api/v1/review/update/{reviewId}")
     public ResponseEntity<ReviewResponse> update(@RequestBody @Valid ReviewUpdateRequest request
-    , @PathVariable Long reviewId) {
+    ,@PathVariable Long reviewId) {
          return ResponseEntity.ok(reviewService.update(reviewId, request));
     }
 
-    @GetMapping("api/v1/review/{reviewId}")
+    // 단건 리뷰 조회
+    @GetMapping("/api/v1/review/{reviewId}")
     public ResponseEntity<ReviewResponse> read(@PathVariable Long reviewId) {
         return ResponseEntity.ok(reviewService.read(reviewId));
     }
 
-    @DeleteMapping("api/v1/review/{reviewId}")
+    // 리뷰 페이징
+    @GetMapping("/api/v1/review")
+    public ResponseEntity<List<ReviewResponse>> readAll(ReviewSearch reviewSearch) {
+        return ResponseEntity.ok(reviewService.getList(reviewSearch));
+
+    }
+
+    @DeleteMapping("/api/v1/review/{reviewId}")
     public ResponseEntity<?> remove(@PathVariable Long reviewId) {
         reviewService.remove(reviewId);
         return ResponseEntity.ok("리뷰 삭제 성공");
