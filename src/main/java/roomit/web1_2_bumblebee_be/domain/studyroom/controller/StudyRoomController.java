0736293fd579fd8bf0dbc844f4540a3f6efbea99
mingeun <164311387+Song-min-geun@users.noreply.github.com>
2024-11-21 14:27:27 +0900

    :sparkles: Feat : StudyRoom Entity,Service, Controller
    Signed-off-by: mingeun <164311387+Song-min-geun@users.noreply.github.com>

--- src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/controller/StudyRoomController.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/controller/StudyRoomController.java
@@ -1,9 +1,64 @@
 package roomit.web1_2_bumblebee_be.domain.studyroom.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import roomit.web1_2_bumblebee_be.domain.studyroom.dto.request.CreateStudyRoomRequest;
+import roomit.web1_2_bumblebee_be.domain.studyroom.dto.request.FindPossibleStudyRoomRequest;
+import roomit.web1_2_bumblebee_be.domain.studyroom.dto.request.UpdateStudyRoomRequest;
+import roomit.web1_2_bumblebee_be.domain.studyroom.dto.response.FindPossibleStudyRoomResponse;
+import roomit.web1_2_bumblebee_be.domain.studyroom.entity.StudyRoom;
+import roomit.web1_2_bumblebee_be.domain.studyroom.service.StudyRoomService;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class StudyRoomController {
 
-}
+    private final StudyRoomService studyRoomService;
 
+    // 스터디룸 생성
+    @PostMapping
+    public ResponseEntity<String> createStudyRoom(@RequestBody CreateStudyRoomRequest request) {
+        studyRoomService.createStudyRoom(request);
+        return ResponseEntity.status(201).body("스터디룸 생성이 성공적으로 완료되었습니다.");  // 201 OK 반환
+    }
+
+    // 스터디룸 조회 (전체)
+    @GetMapping
+    public ResponseEntity<List<StudyRoom>> getAllStudyRooms() {
+        List<StudyRoom> studyRooms = studyRoomService.getAllStudyRooms();
+        return ResponseEntity.ok(studyRooms);
+    }
+
+    // ID로 특정 스터디룸 조회
+    @GetMapping("/studyroom/{id}")
+    public ResponseEntity<StudyRoom> getStudyRoomById(@PathVariable Long id) {
+        StudyRoom studyRoom = studyRoomService.getStudyRoom(id);
+        return ResponseEntity.ok(studyRoom);
+    }
+
+    // 스터디룸 업데이트
+    @PutMapping("/studyroom/{id}")
+    public ResponseEntity<String> updateStudyRoom( @RequestBody UpdateStudyRoomRequest request) {
+        request = new UpdateStudyRoomRequest(request.getStudyRoomId(), request.getTitle(), request.getDescription(), request.getCapacity(), request.getPrice());
+        studyRoomService.updateStudyRoom(request);
+        return ResponseEntity.status(200).body("스터디룸 수정이 성공적으로 완료되었습니다.");
+    }
+
+    // 스터디룸 삭제
+    @DeleteMapping("/studyroom/{id}")
+    public ResponseEntity<String> deleteStudyRoom(@PathVariable Long id) {
+        studyRoomService.deleteStudyRoom(id);
+        return ResponseEntity.status(200).body("스터디룸 삭제가 성공적으로 완료되었습니다.");
+    }
+
+    // 사용 가능한 스터디룸 목록 조회
+    @PostMapping("/studyroom/available")
+    public ResponseEntity<List<FindPossibleStudyRoomResponse>> findAvailableStudyRooms(@RequestBody FindPossibleStudyRoomRequest request) {
+        List<FindPossibleStudyRoomResponse> availableStudyRooms = studyRoomService.findAvailableStudyRooms(request);
+        return ResponseEntity.ok(availableStudyRooms);
+    }
+
+}
--- src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/repository/StudyRoomRepository.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/repository/StudyRoomRepository.java
@@ -1,10 +1,19 @@
 package roomit.web1_2_bumblebee_be.domain.studyroom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import roomit.web1_2_bumblebee_be.domain.studyroom.entity.StudyRoom;
 
+import java.util.List;
+
 @Repository
 public interface StudyRoomRepository extends JpaRepository<StudyRoom,Long> {
+    @Query("SELECT sr FROM StudyRoom sr JOIN sr.workPlaceId wp WHERE wp.workplaceAddress = :workplaceAddress AND sr.capacity >= :capacity AND NOT EXISTS" +
+            "(SELECT r.id FROM Reservation r WHERE r.studyRoom.studyroomId = sr.studyroomId AND ((r.startTime < :endTime AND r.endTime > :startTime) " +
+            " OR (r.startTime <= :startTime AND r.endTime >= :endTime)))")
+    List<StudyRoom> findAvailableStudyRooms(@Param("workplaceAddress") String workplaceAddress,@Param("startTime") String startTime, @Param("endTime") String endTime,@Param("capacity") Integer capacity);
+
 
 }
