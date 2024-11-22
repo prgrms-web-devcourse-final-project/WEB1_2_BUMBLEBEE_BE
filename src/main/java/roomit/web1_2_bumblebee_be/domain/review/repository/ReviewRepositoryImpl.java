1ceb298138ee82082b08392f8f73fc5eeacce2c6
CryingPerson <opnice12@naver.com>
2024-11-22 20:55:11 +0900

    ✨ Feat : ReviewRepositoryImpl getList 변경

--- src/main/java/roomit/web1_2_bumblebee_be/domain/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/review/repository/ReviewRepositoryImpl.java
@@ -1,11 +1,12 @@
 package roomit.web1_2_bumblebee_be.domain.review.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import roomit.web1_2_bumblebee_be.domain.review.dto.request.ReviewSearch;
-import roomit.web1_2_bumblebee_be.domain.review.dto.response.ReviewResponse;
 import roomit.web1_2_bumblebee_be.domain.review.entity.QReview;
 import roomit.web1_2_bumblebee_be.domain.review.entity.Review;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ReviewRepositoryImpl implements ReviewRepositoryCustom{
@@ -16,12 +17,31 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom{
         this.jpaQueryFactory = jpaQueryFactory;
     }
 
+
+
+
     @Override
     public List<Review> getList(ReviewSearch reviewSearch) {
-        return jpaQueryFactory.selectFrom(QReview.review)
+        QReview review = QReview.review;
+        if (reviewSearch.getLastId() != null) {
+            boolean exists = jpaQueryFactory.selectOne()
+                    .from(review)
+                    .where(review.reviewId.eq(reviewSearch.getLastId()))
+                    .fetchFirst() != null;
+
+            // lastId가 존재하지 않으면 빈 리스트 반환
+            if (!exists) {
+                return Collections.emptyList();
+            }
+        }
+        BooleanExpression cursorCondition = reviewSearch.getLastId() == null
+                ? null
+                : review.reviewId.lt(reviewSearch.getLastId());
+
+        return jpaQueryFactory.selectFrom(review)
+                .where(cursorCondition)
                 .limit(reviewSearch.getSize())
-                .offset(reviewSearch.getOffset())
-                .orderBy(QReview.review.reviewId.desc())
+                .orderBy(review.reviewId.desc()) // 최신순으로 정렬
                 .fetch();
     }
 }
