86b41dbe49f090fee1d76917f0afd514eea663ea
CryingPerson <opnice12@naver.com>
2024-11-22 20:49:48 +0900

    ♻️ Refactor : ReviewControllerTest 코드수정

--- src/test/java/roomit/web1_2_bumblebee_be/domain/review/controller/ReviewControllerTest.java
+++ src/test/java/roomit/web1_2_bumblebee_be/domain/review/controller/ReviewControllerTest.java
@@ -1,6 +1,7 @@
 package roomit.web1_2_bumblebee_be.domain.review.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -25,6 +28,7 @@ import roomit.web1_2_bumblebee_be.domain.review.repository.ReviewRepository;
 import roomit.web1_2_bumblebee_be.domain.workplace.entity.Workplace;
 import roomit.web1_2_bumblebee_be.domain.workplace.repository.WorkplaceRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -35,6 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @AutoConfigureMockMvc
 @SpringBootTest
+@ActiveProfiles("test")
 class ReviewControllerTest {
 
     @Autowired
@@ -55,30 +60,18 @@ class ReviewControllerTest {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    private LocalDate date = LocalDate.of(2024, 11, 22);
+
+    private Workplace workplace;
+
+    private Member member;
     @BeforeEach
     void setUp() {
-        memberRepository.deleteAll();
         reviewRepository.deleteAll();
+        memberRepository.deleteAll();
         workplaceRepository.deleteAll();
-    }
 
-    @Test
-    @DisplayName("리뷰 등록")
-    void test1() throws Exception{
-
-        Member member = Member.builder()
-                .memberAge(Age.TEN)
-                .memberSex(Sex.FEMALE)
-                .memberPwd("Business1!")
-                .memberEmail("sdsd@naver.com")
-                .memberPhoneNumber("010-3323-2323")
-                .memberNickName("치킨유저")
-                .passwordEncoder(bCryptPasswordEncoder)
-                .build();
-
-        memberRepository.save(member);
-
-        Workplace workplace = Workplace.builder()
+        workplace = Workplace.builder()
                 .workplaceName("사업장")
                 .workplacePhoneNumber("010-1234-1234")
                 .workplaceDescription("사업장 설명")
@@ -86,6 +79,24 @@ class ReviewControllerTest {
                 .workplaceStartTime(LocalDateTime.of(2023, 1, 1, 9, 0))
                 .workplaceEndTime(LocalDateTime.of(2023, 1, 1, 18, 0))
                 .build();
+
+        member = Member.builder()
+                .birthDay(date)
+                .memberSex(Sex.FEMALE)
+                .memberRole(Role.ROLE_USER)
+                .memberPwd("Bass!1123")
+                .memberEmail("asd@naver.com")
+                .memberPhoneNumber("010-3323-2323")
+                .memberNickName("치킨유저")
+                .passwordEncoder(bCryptPasswordEncoder)
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("리뷰 등록")
+    void test1() throws Exception{
+        memberRepository.save(member);
 
         workplaceRepository.save(workplace);
 
@@ -108,26 +119,11 @@ class ReviewControllerTest {
     @DisplayName("리뷰 수정")
     void test2() throws Exception{
 
-        Member member = Member.builder()
-                .memberAge(Age.TEN)
-                .memberSex(Sex.FEMALE)
-                .memberPwd("Business1!")
-                .memberEmail("sdsd@naver.com")
-                .memberPhoneNumber("010-3323-2323")
-                .memberNickName("치킨유저")
-                .passwordEncoder(bCryptPasswordEncoder)
-                .build();
+
 
         memberRepository.save(member);
 
-        Workplace workplace = Workplace.builder()
-                .workplaceName("사업장")
-                .workplacePhoneNumber("010-1234-1234")
-                .workplaceDescription("사업장 설명")
-                .workplaceAddress("대한민국sa")
-                .workplaceStartTime(LocalDateTime.of(2023, 1, 1, 9, 0))
-                .workplaceEndTime(LocalDateTime.of(2023, 1, 1, 18, 0))
-                .build();
+
 
         workplaceRepository.save(workplace);
 
@@ -161,26 +157,7 @@ class ReviewControllerTest {
     @DisplayName("리뷰 조회")
     void test3() throws Exception{
 
-        Member member = Member.builder()
-                .memberAge(Age.TEN)
-                .memberSex(Sex.FEMALE)
-                .memberPwd("Business1!")
-                .memberEmail("sdsd@naver.com")
-                .memberPhoneNumber("010-3323-2323")
-                .memberNickName("치킨유저")
-                .passwordEncoder(bCryptPasswordEncoder)
-                .build();
-
         memberRepository.save(member);
-
-        Workplace workplace = Workplace.builder()
-                .workplaceName("사업장")
-                .workplacePhoneNumber("010-1234-1234")
-                .workplaceDescription("사업장 설명")
-                .workplaceAddress("대한민국sa")
-                .workplaceStartTime(LocalDateTime.of(2023, 1, 1, 9, 0))
-                .workplaceEndTime(LocalDateTime.of(2023, 1, 1, 18, 0))
-                .build();
 
         workplaceRepository.save(workplace);
 
@@ -206,26 +183,7 @@ class ReviewControllerTest {
     @DisplayName("리뷰 삭제")
     void test4() throws Exception{
 
-        Member member = Member.builder()
-                .memberAge(Age.TEN)
-                .memberSex(Sex.FEMALE)
-                .memberPwd("Business1!")
-                .memberEmail("sdsd@naver.com")
-                .memberPhoneNumber("010-3323-2323")
-                .memberNickName("치킨유저")
-                .passwordEncoder(bCryptPasswordEncoder)
-                .build();
-
         memberRepository.save(member);
-
-        Workplace workplace = Workplace.builder()
-                .workplaceName("사업장")
-                .workplacePhoneNumber("010-1234-1234")
-                .workplaceDescription("사업장 설명")
-                .workplaceAddress("대한민국sa")
-                .workplaceStartTime(LocalDateTime.of(2023, 1, 1, 9, 0))
-                .workplaceEndTime(LocalDateTime.of(2023, 1, 1, 18, 0))
-                .build();
 
         workplaceRepository.save(workplace);
 
@@ -246,29 +204,36 @@ class ReviewControllerTest {
         assertEquals(0, reviewRepository.count());
     }
     @Test
-    @DisplayName("리뷰 페이징")
+    @DisplayName("첫 페이지 요청 커서 없는 case")
     void test5() throws Exception{
-
-        Member member = Member.builder()
-                .memberAge(Age.TEN)
-                .memberSex(Sex.FEMALE)
-                .memberPwd("Business1!")
-                .memberEmail("sdsd@naver.com")
-                .memberPhoneNumber("010-3323-2323")
-                .memberNickName("치킨유저")
-                .passwordEncoder(bCryptPasswordEncoder)
-                .build();
 
         memberRepository.save(member);
 
-        Workplace workplace = Workplace.builder()
-                .workplaceName("사업장")
-                .workplacePhoneNumber("010-1234-1234")
-                .workplaceDescription("사업장 설명")
-                .workplaceAddress("대한민국sa")
-                .workplaceStartTime(LocalDateTime.of(2023, 1, 1, 9, 0))
-                .workplaceEndTime(LocalDateTime.of(2023, 1, 1, 18, 0))
-                .build();
+        workplaceRepository.save(workplace);
+
+        List<Review> list = IntStream.rangeClosed(1, 20).mapToObj(i -> Review.builder()
+                .reviewContent("치킨이 안보이네요.." + i)
+                .reviewRating("별점4개" + i)
+                .member(member)
+                .workplace(workplace)
+                .build()).toList();
+
+        reviewRepository.saveAll(list);
+
+        mockMvc.perform(get("/api/v1/review")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(jsonPath("$.nextCursor").isNotEmpty())
+                .andExpect(jsonPath("$.data[9].reviewContent").value("치킨이 안보이네요..11"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("첫번쨰 페이지 요청시 나온 lastId를 사용하여  커사사용해서 페이징 처리")
+    void test6() throws Exception{
+
+        memberRepository.save(member);
 
         workplaceRepository.save(workplace);
 
@@ -281,15 +246,43 @@ class ReviewControllerTest {
 
         reviewRepository.saveAll(list);
 
-//        ReviewSearch reviewSearch = ReviewSearch.builder()
-//                .page(0)
-//                .size(10)
-//                .build();
-
-        mockMvc.perform(get("/api/v1/review?size=10&page=0")
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/review")
                         .contentType(MediaType.APPLICATION_JSON)
                 )
+                .andReturn();
+
+        String json = mvcResult.getResponse().getContentAsString();
+        Long nestCursor = JsonPath.parse(json).read("$.nextCursor", Long.class);
+
+        mockMvc.perform(get("/api/v1/review?lastId="+nestCursor)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(jsonPath("$.nextCursor").isNotEmpty())
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 LastId 사용시 null 반환")
+    void test7() throws Exception{
+
+        memberRepository.save(member);
+
+        workplaceRepository.save(workplace);
+
+        List<Review> list = IntStream.rangeClosed(0, 19).mapToObj(i -> Review.builder()
+                .reviewContent("치킨이 안보이네요.." + i)
+                .reviewRating("별점4개" + i)
+                .member(member)
+                .workplace(workplace)
+                .build()).toList();
+
+        reviewRepository.saveAll(list);
+
+         mockMvc.perform(get("/api/v1/review?lastId=232")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                 .andDo(print());
+
     }
 }
