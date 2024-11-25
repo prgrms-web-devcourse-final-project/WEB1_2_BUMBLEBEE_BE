f8312bee46483fd033f810e8c6a857b672dff844
CryingPerson <opnice12@naver.com>
2024-11-25 12:00:11 +0900

    ♻️ Refactor : ErrorResponse DTO 수정

--- src/main/java/roomit/web1_2_bumblebee_be/global/error/ErrorResponse.java
+++ src/main/java/roomit/web1_2_bumblebee_be/global/error/ErrorResponse.java
@@ -2,41 +2,29 @@ package roomit.web1_2_bumblebee_be.global.error;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Getter
-public class ErrorResponse {
 
-    private final String code;
-    private final String message;
-    private final int status;
-    private final Map<String, String> validation;
+public record ErrorResponse (ErrorCode errorCode,Map<String, String> validation){
 
     @Builder
-    public ErrorResponse(ErrorCode errorCode, Map<String, String> validation) {
-        this.code = errorCode.getCode();
-        this.message = errorCode.getMessage();
-        this.status = errorCode.getStatus();
-        this.validation = validation;
+    public ErrorResponse {
     }
-    @Getter
-    public static class ErrorValidation{
 
-        private final String code;
-        private final String message;
-        private  Map<String, String> validation;
+        public record ErrorValidation(String code, String message, Map<String, String> validation) {
 
-        @Builder
-        public ErrorValidation(String code, String message, Map<String, String> validation) {
-            this.code = code;
-            this.message = message;
-            this.validation = validation != null ? validation : new HashMap<>();
+            @Builder
+            public ErrorValidation(String code, String message, Map<String, String> validation) {
+                this.code = code;
+                this.message = message;
+                this.validation = validation != null ? validation : new HashMap<>();
+            }
+
+            public void addValidation(String fieldName, String errorMessage) {
+                this.validation.put(fieldName, errorMessage);
+            }
         }
-
-        public void addValidation(String fieldName, String errorMessage) {
-            this.validation.put(fieldName, errorMessage);
-        }
-    }
 }
