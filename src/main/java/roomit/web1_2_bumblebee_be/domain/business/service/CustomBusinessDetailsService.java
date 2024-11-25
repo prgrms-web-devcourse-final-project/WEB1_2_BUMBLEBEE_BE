c6e4e0a106d029f638c066530befbc635a232b54
Si-rauis <kkyd7189@gmail.com>
2024-11-25 10:41:28 +0900

    ♻️ Refactor : Business Servic단 예외처리 수정
    불필요한 예외처리 코드 삭제 및 수정C

--- src/main/java/roomit/web1_2_bumblebee_be/domain/business/service/CustomBusinessDetailsService.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/business/service/CustomBusinessDetailsService.java
@@ -6,9 +6,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import roomit.web1_2_bumblebee_be.domain.business.entity.Business;
-import roomit.web1_2_bumblebee_be.domain.business.exception.BusinessNotFound;
 import roomit.web1_2_bumblebee_be.domain.business.repository.BusinessRepository;
 import roomit.web1_2_bumblebee_be.domain.business.dto.CustomBusinessDetails;
+import roomit.web1_2_bumblebee_be.global.error.ErrorCode;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +18,7 @@ public class CustomBusinessDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Business business = businessRepository.findByBusinessEmail(email).orElseThrow(BusinessNotFound::new);
+        Business business = businessRepository.findByBusinessEmail(email).orElseThrow(ErrorCode.BUSINESS_NOT_FOUND::commonException);
 
         if (business != null){
 
