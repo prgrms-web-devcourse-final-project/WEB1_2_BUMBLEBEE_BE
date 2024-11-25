1295897bdde38485a7a3352b53bb87394a10f699
CryingPerson <opnice12@naver.com>
2024-11-25 10:34:45 +0900

    ♻️ Refactor : Review DTO record 변경

--- src/main/java/roomit/web1_2_bumblebee_be/domain/review/dto/request/ReviewRegisterRequest.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/review/dto/request/ReviewRegisterRequest.java
@@ -5,17 +5,11 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
-public class ReviewRegisterRequest {
-    @NotNull(message = "회원 아이디는 필수입니다.")
-    private final Long memberId;
-    @NotNull(message = "리뷰를 적을 사업장 번호를 적어주세요")
-    private final Long workplaceId;
-    @NotBlank(message = "리뷰 내용을 적어주세요.")
-    private final String reviewContent;
-    @NotBlank(message = "별점 입력해 주세요.")
-    private final String reviewRating;
 
+public record ReviewRegisterRequest(@NotNull(message = "회원 아이디는 필수입니다.") Long memberId,
+                                    @NotNull(message = "리뷰를 적을 사업장 번호를 적어주세요") Long workplaceId,
+                                    @NotBlank(message = "리뷰 내용을 적어주세요.") String reviewContent,
+                                    @NotBlank(message = "별점 입력해 주세요.") String reviewRating) {
     @Builder
     public ReviewRegisterRequest(Long memberId, Long workplaceId, String reviewContent, String reviewRating) {
         this.memberId = memberId;
