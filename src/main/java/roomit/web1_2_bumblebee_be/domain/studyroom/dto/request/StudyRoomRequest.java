2e09d58a0986c05678593e3d0f36acc50c634c70
geun <minkeon44@naver.com>
2024-11-25 15:17:45 +0900

    :recycle: Refactor : dto 리팩토링

--- src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/dto/request/StudyRoomRequest.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/dto/request/StudyRoomRequest.java
@@ -1,29 +1,14 @@
 package roomit.web1_2_bumblebee_be.domain.studyroom.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Getter;
 import roomit.web1_2_bumblebee_be.domain.studyroom.entity.StudyRoom;
 
-@Getter
-public class StudyRoomRequest {
-    @NotBlank(message = "스터디룸 이름은 필수입니다.")
-    private String title;
-    @NotBlank(message = "스터디룸 설명은 필수입니다.")
-    private String description;
-    @NotBlank(message = "스터디룸 수용인원은 필수입니다.")
-    private Integer capacity;
-    @NotBlank(message = "금액은 필수입니다.")
-    private Integer price;
-
-    @Builder
-    public StudyRoomRequest(String title, String description, Integer capacity, Integer price) {
-        this.title = title;
-        this.description = description;
-        this.capacity = capacity;
-        this.price = price;
-    }
-
+public record StudyRoomRequest(
+        @NotBlank(message = "스터디룸 이름은 필수입니다.") String title,
+        @NotBlank(message = "스터디룸 설명은 필수입니다.") String description,
+        @NotBlank(message = "스터디룸 수용인원은 필수입니다.") Integer capacity,
+        @NotBlank(message = "금액은 필수입니다.") Integer price
+) {
     // toEntity 메서드 추가
     public StudyRoom toEntity() {
         return StudyRoom.builder()
--- src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/dto/response/StudyRoomResponse.java
+++ src/main/java/roomit/web1_2_bumblebee_be/domain/studyroom/dto/response/StudyRoomResponse.java
@@ -1,23 +1,22 @@
 package roomit.web1_2_bumblebee_be.domain.studyroom.dto.response;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
 import roomit.web1_2_bumblebee_be.domain.studyroom.entity.StudyRoom;
 
+public record StudyRoomResponse (
+    String title,
+    String description,
+    String imageUrl,
+    Integer price,
+    Integer capacity
+){
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class StudyRoomResponse {
-
-    private Long studyRoomId; // 변수명 변경
-    private String title;
-    private String description;
-
-
-    private void setStudyRoomFields(StudyRoom studyRoom) {
-        this.studyRoomId = studyRoom.getStudyroomId();
-        this.title = studyRoom.getTitle();
-        this.description = studyRoom.getDescription();
+    public static StudyRoomResponse from(StudyRoom studyRoom) {
+        return new StudyRoomResponse(
+                studyRoom.getTitle(),
+                studyRoom.getDescription(),
+                studyRoom.getImageUrl(),
+                studyRoom.getPrice(),
+                studyRoom.getCapacity()
+                );
     }
 }
