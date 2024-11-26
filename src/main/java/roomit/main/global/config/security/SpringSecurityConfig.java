15a06cc16f6347ef08ebe8348e8bdf6f92afaab2
Si-rauis <kkyd7189@gmail.com>
2024-11-26 12:46:52 +0900

    ✨ Feat : Local 환경 Test 툴 연결 완료

--- src/main/java/roomit/main/global/config/security/SpringSecurityConfig.java
+++ src/main/java/roomit/main/global/config/security/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
