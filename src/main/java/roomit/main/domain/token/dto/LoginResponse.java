4b0befe7e7b5f931051e541b716ff911b7d8ff0a
Si-rauis <kkyd7189@gmail.com>
2024-11-26 16:41:10 +0900

    ♻️ Refactor : 잘못된 BusinessController 도메인 수정 및 Login DTO 일부 수정 <나중에 추가 수정 필요>

--- src/main/java/roomit/main/domain/token/dto/LoginResponse.java
+++ src/main/java/roomit/main/domain/token/dto/LoginResponse.java
@@ -2,9 +2,12 @@ package roomit.main.domain.token.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
     private String token;
+
 }
