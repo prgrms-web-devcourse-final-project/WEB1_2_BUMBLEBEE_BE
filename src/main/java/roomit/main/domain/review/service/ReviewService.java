e7a2304547c5fb1c73148593cab41d8cc2dd7109
CryingPerson <opnice12@naver.com>
2024-11-27 14:14:15 +0900

    🗑️ chore : ReviewService 나만 조회

--- src/main/java/roomit/main/domain/review/service/ReviewService.java
+++ src/main/java/roomit/main/domain/review/service/ReviewService.java
@@ -14,6 +14,7 @@ import roomit.main.domain.workplace.entity.Workplace;
 import roomit.main.domain.workplace.repository.WorkplaceRepository;
 import roomit.main.global.error.ErrorCode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -57,11 +58,17 @@ public class ReviewService {
         return new ReviewResponse(review);
     }
 
-    public ReviewResponse read(Long reviewId) {
-        Review review = reviewRepository.findById(reviewId)
-                .orElseThrow(ErrorCode.REVIEW_NOT_FOUND::commonException);
+    public List<ReviewResponse> read(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(ErrorCode.MEMBER_NOT_FOUND::commonException);
 
-        return new ReviewResponse(review);
+        List<Review> reviews = member.getReviews();
+        List<ReviewResponse> responses = new ArrayList<>();
+
+        for(Review review : reviews) {
+           responses.add(new ReviewResponse(review));
+        }
+        return responses;
     }
 
     public void remove(Long reviewId) {
