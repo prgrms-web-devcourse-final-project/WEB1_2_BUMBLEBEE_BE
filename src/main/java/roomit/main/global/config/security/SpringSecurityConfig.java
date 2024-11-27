51c050af98418c4c9ee85b315b429903d06af56a
bung-dev <wndgh26@gmail.com>
2024-11-27 15:30:11 +0900

    :sparkles: feat: SpringSecurity 경로별 인가 작업

--- src/main/java/roomit/main/global/config/security/SpringSecurityConfig.java
+++ src/main/java/roomit/main/global/config/security/SpringSecurityConfig.java
@@ -170,7 +170,6 @@ public class SpringSecurityConfig {
 
                         .anyRequest().authenticated());
 
-
         http
                 .addFilterBefore(new JWTFilter(jwtUtil,bCryptPasswordEncoder(),memberDetailsService,businessDetailsService), LoginFilter.class);
 
