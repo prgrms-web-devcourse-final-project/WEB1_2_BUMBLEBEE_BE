6aeb756036d2c8ea7ea4eb6550ec99f296b95984
CryingPerson <opnice12@naver.com>
2024-11-29 12:44:40 +0900

    ♻️ Refactor : ReviewRepositoryImpl 수정

--- src/main/java/roomit/main/domain/review/repository/ReviewRepository.java
+++ src/main/java/roomit/main/domain/review/repository/ReviewRepository.java
@@ -3,5 +3,7 @@ package roomit.main.domain.review.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import roomit.main.domain.review.entity.Review;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> , ReviewRepositoryCustom{
 }
