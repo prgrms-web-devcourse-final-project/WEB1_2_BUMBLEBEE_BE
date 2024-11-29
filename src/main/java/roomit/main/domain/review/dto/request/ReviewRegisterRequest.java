90e004d4b0ad9bb325b077e652063c26592ec96f
CryingPerson <opnice12@naver.com>
2024-11-29 12:44:30 +0900

    ♻️ Refactor : ReviewDTO 수정

--- src/main/java/roomit/main/domain/review/dto/request/ReviewRegisterRequest.java
+++ src/main/java/roomit/main/domain/review/dto/request/ReviewRegisterRequest.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 
-public record ReviewRegisterRequest(
-                                    @NotNull(message = "리뷰를 적을 사업장 번호를 적어주세요") Long workplaceId,
+public record ReviewRegisterRequest(@NotNull(message = "예약 id는 필수입니다.") Long reservatinId,
+                                    @NotBlank(message = "사업장 이름") String workPlaceName,
                                     @NotBlank(message = "리뷰 내용을 적어주세요.") String reviewContent,
                                     @NotNull(message = "별점 입력해 주세요.") Double reviewRating) {
     @Builder
