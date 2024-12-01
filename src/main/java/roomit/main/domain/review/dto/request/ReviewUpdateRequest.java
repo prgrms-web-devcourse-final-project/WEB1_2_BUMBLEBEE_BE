faf12472c54d43f7efdaf8790dab6e6552c812c3
Si-rauis <kkyd7189@gmail.com>
2024-12-01 16:52:50 +0900

    ♻️ Refactor : Review UpdateAt null허용

--- src/main/java/roomit/main/domain/review/dto/request/ReviewUpdateRequest.java
+++ src/main/java/roomit/main/domain/review/dto/request/ReviewUpdateRequest.java
@@ -4,11 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
-
 @Builder
-public record ReviewUpdateRequest(
-                                  @NotBlank(message = "리뷰 내용을 적어주세요.") String reviewContent,
-                                  @NotNull(message = "별점 입력해 주세요.") int reviewRating,
-                                  LocalDateTime reviewUpdateTime) {
+public record ReviewUpdateRequest(@NotBlank(message = "리뷰 내용을 적어주세요.") String reviewContent,
+                                  @NotNull(message = "별점 입력해 주세요.") int reviewRating) {
 }
