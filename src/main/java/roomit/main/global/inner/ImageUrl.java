37ec01b293ab374f789c1f0fcf92124d391be7af
Si-rauis <kkyd7189@gmail.com>
2024-12-02 17:54:19 +0900

    ♻️ Refactor : ImageUrl 정규표현식 수정
    기하급수적인 백트래킹으로 인한 서비스 거부(DoS) 취약점에 노출

--- src/main/java/roomit/main/global/inner/ImageUrl.java
+++ src/main/java/roomit/main/global/inner/ImageUrl.java
@@ -16,9 +16,10 @@ public class ImageUrl {
     // 이미지 URL 검증을 위한 정규식
     public static final String REGEX = "^[a-zA-Z0-9가-힣\\s/.-]+$";
     public static final String ERR_MSG = "영문, 숫자, 한글, 공백, /, ., -만 포함해야 합니다.";
-    public static final String DB_REGEX = "^https://[a-zA-Z0-9.-]+\\.s3\\.[a-zA-Z0-9.-]+\\.amazonaws\\.com(?:/[a-zA-Z0-9가-힣\\s/.-]*)?$";
+    public static final String DB_REGEX = "^https://.*$";
     public static final String DB_ERR_MSG = "이미지 URL은 https://로 시작하며, 올바른 S3 URL 형식이어야 합니다.";
-    private static final Pattern PATTERN = Pattern.compile(DB_REGEX);
+    public static final Pattern PATTERN = Pattern.compile(DB_REGEX);
+
 
     // S3 URL의 기본 부분 (버킷 이름과 리전 포함)
     private static final String S3_BASE_URL = "https://{bucket-name}.s3.{region}.amazonaws.com/";
