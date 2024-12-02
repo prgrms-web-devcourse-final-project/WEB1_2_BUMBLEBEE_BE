04cbe24644f760726cfc15a4676b5046cafed3c4
Si-rauis <kkyd7189@gmail.com>
2024-12-02 14:49:58 +0900

    🗑️ Chore : 트랜잭션 설정으로 필요없는 save구문 삭제

--- src/main/java/roomit/main/global/inner/AwsProperties.java
+++ src/main/java/roomit/main/global/inner/AwsProperties.java
@@ -1,29 +1,18 @@
 package roomit.main.global.inner;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "amazon.aws")
 public class AwsProperties {
 
+  // getters and setters
   private String bucket;
   private String region;
 
-  // getters and setters
-  public String getBucket() {
-    return bucket;
-  }
-
-  public void setBucket(String bucket) {
-    this.bucket = bucket;
-  }
-
-  public String getRegion() {
-    return region;
-  }
-
-  public void setRegion(String region) {
-    this.region = region;
-  }
 }
