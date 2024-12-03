1cbaba8c2b47f2254f8a0cac87d52510668bf74e
Si-rauis <kkyd7189@gmail.com>
2024-12-03 19:45:49 +0900

    🗑️ Chore : Review 변수 스펠링 실수 수정

--- src/main/java/roomit/main/domain/review/dto/request/ReviewRegisterRequest.java
+++ src/main/java/roomit/main/domain/review/dto/request/ReviewRegisterRequest.java
@@ -3,13 +3,11 @@ package roomit.main.domain.review.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import roomit.main.domain.member.entity.Member;
 import roomit.main.domain.reservation.entity.Reservation;
 import roomit.main.domain.review.entity.Review;
 
 @Builder
-public record ReviewRegisterRequest(@NotNull(message = "예약 id는 필수입니다.") Long reservatinId,
+public record ReviewRegisterRequest(@NotNull(message = "예약 id는 필수입니다.") Long reservationId,
                                     @NotBlank(message = "사업장 이름") String workPlaceName,
                                     @NotBlank(message = "리뷰 내용을 적어주세요.") String reviewContent,
                                     @NotNull(message = "별점 입력해 주세요.") int reviewRating) {
