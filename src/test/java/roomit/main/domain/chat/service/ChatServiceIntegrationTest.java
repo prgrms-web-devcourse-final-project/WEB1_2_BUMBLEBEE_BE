32b1446fa97c1d4403b3b9c01944b17c2d7d735d
usingjun <usingjun99@gmail.com>
2024-12-05 10:36:02 +0900

    :sparkles:  Feat : 채팅방 조회 및 생성 테스트

--- src/test/java/roomit/main/domain/chat/service/ChatServiceIntegrationTest.java
+++ src/test/java/roomit/main/domain/chat/service/ChatServiceIntegrationTest.java
@@ -1,45 +1,49 @@
 package roomit.main.domain.chat.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-import roomit.main.domain.business.dto.request.BusinessRegisterRequest;
 import roomit.main.domain.business.entity.Business;
 import roomit.main.domain.business.repository.BusinessRepository;
-import roomit.main.domain.business.service.BusinessService;
-import roomit.main.domain.chat.chatmessage.dto.ChatMessageRequest;
-import roomit.main.domain.chat.chatmessage.entity.ChatMessage;
 import roomit.main.domain.chat.chatmessage.repository.ChatMessageRepository;
 import roomit.main.domain.chat.chatmessage.service.ChatService;
-import roomit.main.domain.chat.chatroom.entity.ChatRoom;
+import roomit.main.domain.chat.chatroom.dto.ChatRoomBusinessResponse;
+import roomit.main.domain.chat.chatroom.dto.ChatRoomMemberResponse;
+import roomit.main.domain.chat.chatroom.dto.ChatRoomResponse;
 import roomit.main.domain.chat.chatroom.repositoroy.ChatRoomRepository;
+import roomit.main.domain.chat.chatroom.service.ChatRoomService;
+import roomit.main.domain.member.dto.CustomMemberDetails;
 import roomit.main.domain.member.entity.Member;
 import roomit.main.domain.member.entity.Sex;
 import roomit.main.domain.member.repository.MemberRepository;
+import roomit.main.domain.studyroom.entity.StudyRoom;
+import roomit.main.domain.studyroom.repository.StudyRoomRepository;
+import roomit.main.domain.workplace.entity.Workplace;
+import roomit.main.domain.workplace.repository.WorkplaceRepository;
+import roomit.main.global.exception.CommonException;
+import roomit.main.global.service.ImageService;
 
-import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Transactional
 @SpringBootTest
 @ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ChatServiceIntegrationTest {
 
     @Autowired
     private ChatService chatService;
+
+    @Autowired
+    private ChatRoomService chatRoomService;
 
     @Autowired
     private ChatRoomRepository chatRoomRepository;
@@ -54,22 +58,34 @@ class ChatServiceIntegrationTest {
     private MemberRepository memberRepository;
 
     @Autowired
+    private StudyRoomRepository studyRoomRepository;
+
+    @Autowired
+    private WorkplaceRepository workplaceRepository;
+
+    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ImageService imageService;
 
     private Business business;
 
     private Member member;
 
-    @BeforeEach
+    private Workplace workplace;
+
+    private StudyRoom studyRoom;
+
+    @BeforeAll
     void setUp() {
         businessRepository.deleteAll();
         memberRepository.deleteAll();
+        chatMessageRepository.deleteAll();
+        chatRoomRepository.deleteAll();
 
         String email = "business12@gmail.com";
 
@@ -84,7 +100,7 @@ class ChatServiceIntegrationTest {
 
         businessRepository.save(business);
 
-        member =  Member.builder()
+        member = Member.builder()
                 .birthDay(LocalDate.of(2024, 11, 22))
                 .memberSex(Sex.FEMALE)
                 .memberPwd("Business1!")
@@ -95,10 +111,34 @@ class ChatServiceIntegrationTest {
                 .build();
 
         memberRepository.save(member);
+
+        workplace = Workplace.builder()
+                .workplaceName("Workplace")
+                .workplacePhoneNumber("0507-1234-5678")
+                .workplaceDescription("사업장 설명")
+                .workplaceAddress("서울 중구 장충단로 247 굿모닝시티 8층")
+                .imageUrl(imageService.createImageUrl("Workplace"))
+                .workplaceStartTime(LocalTime.of(9, 0))
+                .workplaceEndTime(LocalTime.of(18, 0))
+                .business(business)
+                .build();
+        workplaceRepository.save(workplace);
+
+        studyRoom = StudyRoom.builder()
+                .studyRoomName("Test Room")
+                .description("A test room")
+                .capacity(10)
+                .price(100)
+                .imageUrl(imageService.createImageUrl("Workplace/Test Room"))
+                .workplace(workplace)
+                .build();
+
+        studyRoomRepository.save(studyRoom);
     }
 
 
     @Test
+    @DisplayName("redis 연결 테스트")
     void testRedisConnection() {
         redisTemplate.opsForValue().set("test-key", "test-value");
         String value = redisTemplate.opsForValue().get("test-key");
@@ -107,52 +147,84 @@ class ChatServiceIntegrationTest {
     }
 
     @Test
-    void testSerialization() throws IOException {
-        // Given
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    @DisplayName("채팅방 생성 테스트 - 성공")
+    void createChatRoom() {
+        chatRoomRepository.deleteAll();
 
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-
-        ChatMessageRequest originalMessage = new ChatMessageRequest(1L, "Tester", "Hello, Redis!", LocalDateTime.now(), "member");
-
-        // Serialize
-        byte[] serialized = serializer.serialize(originalMessage);
-        System.out.println("Serialized data: " + new String(serialized));
-
-        // Deserialize explicitly using ObjectMapper
-        ChatMessageRequest deserialized = objectMapper.readValue(serialized, ChatMessageRequest.class);
-        System.out.println("Deserialized object: " + deserialized);
-
-        // Then
-        assertThat(deserialized).isEqualTo(originalMessage);
-    }
-
-
-
-
-    @Test
-    void testSendMessageAndFlush() {
-        // Given
-        ChatRoom chatRoom = chatRoomRepository.save(new ChatRoom(business, member));
-        ChatMessageRequest request = new ChatMessageRequest(chatRoom.getRoomId(), "Tester", "Hello, Redis!", LocalDateTime.now(), "member");
-        System.out.println("ChatRoom saved with ID: " + chatRoom.getRoomId());
+        //Given
+        Long memberId = member.getMemberId();
+        Long studyRoomId = studyRoom.getStudyRoomId();
 
         // When
-        chatService.sendMessage(request);
-        System.out.println("Message sent to Redis: " + request);
+        chatRoomService.create(memberId, studyRoomId);
+        //then
+        boolean chatRoomExists = chatRoomRepository.existsChatRoomByMemberIdAndBusinessId(memberId, business.getBusinessId());
+        Assertions.assertTrue(chatRoomExists, "ChatRoom이 생성되지 않았습니다.");
 
-        chatService.flushMessagesToDatabase(chatRoom.getRoomId());
-        System.out.println("Flushed messages from Redis to MySQL for Room ID: " + chatRoom.getRoomId());
+        List<ChatRoomResponse> createMemberChatRoomList = chatRoomRepository.findChatRoomByMembersId(memberId);
 
-        // Then
-        List<ChatMessage> messages = chatMessageRepository.findAll();
-        System.out.println("Messages in MySQL: " + messages);
-        assertThat(messages).isNotEmpty(); // 데이터가 존재하는지 확인
+//        Assertions.assertEquals(1,createMemberChatRoomList.size());
+        for (ChatRoomResponse chatRoom : createMemberChatRoomList) {
+            ChatRoomMemberResponse chatRoomMemberResponse = (ChatRoomMemberResponse) chatRoom;
+            Assertions.assertEquals(chatRoomMemberResponse.businessId(), business.getBusinessId());
+        }
 
-        ChatMessage savedMessage = messages.get(0);
-        assertThat(savedMessage.getSender()).isEqualTo("Tester");
-        assertThat(savedMessage.getContent()).isEqualTo("Hello, Redis!");
+        List<ChatRoomResponse> createBusinessChatRoomList = chatRoomRepository.findChatRoomByBusinessId(business.getBusinessId());
+
+//        Assertions.assertEquals(1,createBusinessChatRoomList.size());
+        for (ChatRoomResponse chatRoom : createBusinessChatRoomList) {
+            ChatRoomBusinessResponse chatRoomMemberResponse = (ChatRoomBusinessResponse) chatRoom;
+            Assertions.assertEquals(chatRoomMemberResponse.memberId(), memberId);
+        }
     }
+
+    @Test
+    @DisplayName("이미 존재하는 채팅방")
+    void createChatRoom_already() {
+        //Given
+        Long memberId = member.getMemberId();
+        Long studyRoomId = studyRoom.getStudyRoomId();
+        chatRoomService.create(memberId, studyRoomId);
+
+//        //When & Then
+//        Assertions.assertThrows(CommonException.class, () -> {
+//            chatRoomService.create(memberId, studyRoomId);
+//        });
+    }
+
+    @Test
+    @DisplayName("채팅방 조회 - member")
+    void getRooms() {
+        //Given
+        Long memberId = member.getMemberId();
+        CustomMemberDetails customMemberDetails = new CustomMemberDetails(member);
+
+        //When
+
+    }
+
+//    @Test
+//    void testSendMessage() {
+//
+//        // Given
+//        ChatRoom chatRoom = chatRoomRepository.save(new ChatRoom(business, member));
+//        ChatMessageRequest request = new ChatMessageRequest(chatRoom.getRoomId(), "Tester", "Hello, Redis!", LocalDateTime.now(), "member");
+//        System.out.println("ChatRoom saved with ID: " + chatRoom.getRoomId());
+//
+//        // When
+//        chatService.sendMessage(request);
+//        System.out.println("Message sent to Redis: " + request);
+//
+//        chatService.flushMessagesToDatabase(chatRoom.getRoomId());
+//        System.out.println("Flushed messages from Redis to MySQL for Room ID: " + chatRoom.getRoomId());
+//
+//        // Then
+//        List<ChatMessage> messages = chatMessageRepository.findAll();
+//        System.out.println("Messages in MySQL: " + messages);
+//        assertThat(messages).isNotEmpty(); // 데이터가 존재하는지 확인
+//
+//        ChatMessage savedMessage = messages.get(0);
+//        assertThat(savedMessage.getSender()).isEqualTo("Tester");
+//        assertThat(savedMessage.getContent()).isEqualTo("Hello, Redis!");
+//    }
 }
