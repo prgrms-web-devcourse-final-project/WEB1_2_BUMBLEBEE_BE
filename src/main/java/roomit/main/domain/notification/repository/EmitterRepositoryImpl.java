4591f843cf0438434a1c5ee3214833c52d22e723
CryingPerson <opnice12@naver.com>
2024-12-06 10:04:12 +0900

    ✨ Feat : EmitterRepository 캐쉬 저장 추가

--- src/main/java/roomit/main/domain/notification/repository/EmitterRepositoryImpl.java
+++ src/main/java/roomit/main/domain/notification/repository/EmitterRepositoryImpl.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 @Repository
 public class EmitterRepositoryImpl implements EmitterRepository {
     private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
-    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
+    private final Map<Long, Object> eventCache = new ConcurrentHashMap<>();
 
     @Override
     public void save(Long emitterId, SseEmitter sseEmitter) {
@@ -36,6 +36,14 @@ public class EmitterRepositoryImpl implements EmitterRepository {
     @Override
     public void deleteAllEventCache() {
         eventCache.clear();
+    }
+
+    public void saveEventCache(Long businessId, Object eventData) {
+        eventCache.put(businessId, eventData);
+    }
+
+    public Object getEventCache(Long businessId) {
+        return eventCache.get(businessId);
     }
 
 
