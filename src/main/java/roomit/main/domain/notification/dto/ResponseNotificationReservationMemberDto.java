6d66dd1750c9edf2c1e987a3c446cd32a078d0d5
CryingPerson <opnice12@naver.com>
2024-12-08 00:10:10 +0900

    ✨ Feat : 멤버 예약 알림 DTO 추가

---
+++ src/main/java/roomit/main/domain/notification/dto/ResponseNotificationReservationMemberDto.java
@@ -0,0 +1,39 @@
+package roomit.main.domain.notification.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import roomit.main.domain.notification.entity.MemberNotification;
+import roomit.main.domain.notification.entity.Notification;
+import roomit.main.domain.notification.entity.NotificationType;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class ResponseNotificationReservationMemberDto {
+    private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime createdAt;
+    private Long workplaceId;
+    private NotificationType notificationType;
+    private Long price;
+
+    @Builder
+    public ResponseNotificationReservationMemberDto(MemberNotification notification) {
+        this.content = notification.getContent();
+        this.createdAt = notification.getCreatedAt();
+        this.workplaceId = notification.getWorkplaceId();
+        this.notificationType = notification.getNotificationType();
+        this.price = notification.getPrice();
+    }
+
+    public static ResponseNotificationReservationMemberDto fromEntityReservationtoMember(MemberNotification memberNotification) {
+        return new ResponseNotificationReservationMemberDto(
+                memberNotification
+        );
+    }
+}
