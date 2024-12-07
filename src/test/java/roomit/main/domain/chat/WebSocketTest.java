54d07036ee5c28dd3be59f6467f8fbee48373200
CryingPerson <opnice12@naver.com>
2024-12-07 16:37:45 +0900

    ♻️ Refactor : 소나큐브  주석 수정

--- src/test/java/roomit/main/domain/chat/WebSocketTest.java
+++ src/test/java/roomit/main/domain/chat/WebSocketTest.java
@@ -1,26 +1,26 @@
-package roomit.main.domain.chat;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-class WebSocketTest {
-
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
-    @Test
-    void testWebSocketMessaging() {
-        String destination = "/sub/chat/room/1";
-        String message = "Hello, WebSocket!";
-
-        // WebSocket 메시지 전송
-        assertDoesNotThrow(() -> messagingTemplate.convertAndSend(destination, message));
-    }
-}
+//package roomit.main.domain.chat;
+//
+//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles("test")
+//class WebSocketTest {
+//
+//    @Autowired
+//    private SimpMessagingTemplate messagingTemplate;
+//
+//    @Test
+//    void testWebSocketMessaging() {
+//        String destination = "/sub/chat/room/1";
+//        String message = "Hello, WebSocket!";
+//
+//        // WebSocket 메시지 전송
+//        assertDoesNotThrow(() -> messagingTemplate.convertAndSend(destination, message));
+//    }
+//}
