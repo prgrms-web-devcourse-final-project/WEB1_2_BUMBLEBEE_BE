29919e89f4dff118dee068f1c6c9060421dd104a
CryingPerson <opnice12@naver.com>
2025-01-10 14:51:16 +0900

    ✨ Feat : 알림 3개월 지난 알림 삭제 Repository 추가

--- src/main/java/roomit/main/domain/notification/repository/NotificationRepository.java
+++ src/main/java/roomit/main/domain/notification/repository/NotificationRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.repository.query.Param;
 import roomit.main.domain.business.entity.Business;
 import roomit.main.domain.notification.entity.Notification;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     @Query("SELECT n FROM Notification n JOIN FETCH n.business b WHERE b.businessId = :businessId ORDER BY n.createdAt DESC")
     List<Notification> findNotificationsByBusinessId(@Param("businessId") Long businessId);
+
+    @Query("DELETE FROM Notification r WHERE r.createdAt < :cubeforeDate")
+    void deleteByCreatedAtBefore(@Param("cubeforeDate") LocalDateTime cubeforeDate);
 
 }
