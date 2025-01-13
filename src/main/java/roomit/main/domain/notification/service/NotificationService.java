6a62df03e0728d7c1d55fbd521535e9a266dc23d
CryingPerson <opnice12@naver.com>
2025-01-13 14:42:48 +0900

    🔥 Remove : 불필요한 주석 삭제

--- src/main/java/roomit/main/domain/notification/service/NotificationService.java
+++ src/main/java/roomit/main/domain/notification/service/NotificationService.java
@@ -7,11 +7,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import roomit.main.domain.business.entity.Business;
-import roomit.main.domain.business.repository.BusinessRepository;
-import roomit.main.domain.member.dto.request.MemberUpdateRequest;
-import roomit.main.domain.member.entity.Member;
-import roomit.main.domain.member.repository.MemberRepository;
 import roomit.main.domain.notification.dto.ResponseNotificationDto;
 import roomit.main.domain.notification.dto.ResponseNotificationReservationDto;
 import roomit.main.domain.notification.dto.ResponseNotificationReservationMemberDto;
@@ -22,7 +17,6 @@ import roomit.main.domain.notification.repository.EmitterRepository;
 import roomit.main.domain.notification.repository.MemberNotificationRepository;
 import roomit.main.domain.notification.repository.NotificationRepository;
 import roomit.main.domain.notification.repository.ReviewNotificationRepository;
-import roomit.main.domain.workplace.repository.WorkplaceRepository;
 import roomit.main.global.error.ErrorCode;
 import roomit.main.global.service.FileLocationService;
 
@@ -178,15 +172,16 @@ public class NotificationService {
 
     // 3개월이 지난 사업자 리뷰 알림 제거
     @Scheduled(cron = "0 0 3 * * ?")
-    public void deleteOldReviewNotifications(){
+    public void deleteOldReviewNotifications() {
         reviewNotificationRepository.deleteByCreatedAtBefore(LocalDateTime.now().minusMonths(3));
     }
 
     // 3개월이 지난 사업자 예약 알림 제거
     @Scheduled(cron = "0 0 3 * * ?")
-    public void deleteOldReservationNotifications(){
+    public void deleteOldReservationNotifications() {
         notificationRepository.deleteByCreatedAtBefore(LocalDateTime.now().minusMonths(3));
     }
+
     // 3개월이 지난 회원 예약 알림 제거
     @Scheduled(cron = "0 0 3 * * ?")
     public void deleteOldMemberReservationNotifications() {
